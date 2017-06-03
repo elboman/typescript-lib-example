fc25f637a47654663c453095720f27cbb41efcff
bbottema <b.bottema@projectnibble.org>
2017-06-03 06:30:43 +0200

    updated to Webpack 2.4.1

--- webpack.config.js
+++ webpack.config.js
@@ -39,19 +39,18 @@ var config = {
             include: /\.min\.js$/,
         })
     ],
+
+
+    // Webpack doesn't understand TypeScript files and a loader is needed.
     module: {
-        // Webpack doesn't understand TypeScript files and a loader is needed. `node_modules` folder is excluded in order to prevent problems with
-        // the library dependencies, as well as `__tests__` folders that contain the tests for the library
-        loaders: [{
-            test: /\.tsx?$/,
-            loader: 'awesome-typescript-loader',
-            exclude: /node_modules/,
-            query: {
-                // we don't want any declaration file in the bundles folder since it wouldn't be of any use ans the source
-                // map already include everything for debugging
-                declaration: false,
+        rules: [
+            {
+                test: /\.ts$/,
+                use: [
+                    {loader: 'awesome-typescript-loader'}
+                ]
             }
-        }]
+        ]
     }
 };
 
